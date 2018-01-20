54126e3e176d4a57318fc314749e3e814dce0b33
Christophe Noël <christophe@taotesting.com>
2018-01-20 17:08:27 +0100

    fixed eslint file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,16 @@
 module.exports = {
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 2017
+    },
     "env": {
+        "es6": true,
         "node": true
+    },
+    "rules": {
+        "no-useless-escape": "off",
+        "no-shadow": "error",
+        "no-trailing-spaces": "error",
+        "semi": ["error", "always"]
     }
 };
